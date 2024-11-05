bef70078a210efe0e75d964b0a02fe6ddadbbe5c
Mitchell Tillman <mtillman14@gmail.com>
2024-11-04 23:58:39 -0600

    Tiny change to tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,11 +6,15 @@ from dag_dataset.examples import load_test_table
 
 from dagrunner.run_dag import DagRunner
 
-def test_dagrunner():
+def create_dagrunner():
     package_name = "frame_range_no_nan"
     dag = compile_dag(package_name)
     dataset = load_test_table()
-    dagrunner = DagRunner(dag, dataset)
+    dagrunner = DagRunner(dag, dataset)  
+    return dagrunner  
+
+def test_dagrunner():
+    dagrunner = create_dagrunner()
     print(dagrunner)
 
 if __name__ == '__main__':
